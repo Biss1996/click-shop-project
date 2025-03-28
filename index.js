356eac96633e20c49b0f0c4cceb04501abe49e64
Bismark <bettkipkoech45@gmail.com>
2025-03-28 09:08:13 +0300

    fetch products css

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+document.getElementById('fetchButton').addEventListener('click', fetchProducts);
+document.getElementById('viewCartButton').addEventListener('click', viewCart);
+document.getElementById('searchButton').addEventListener('click', searchProducts);
+document.getElementById('filterButton').addEventListener('click', filterProducts);
+
+let cart = [];
+let allProducts = [];
+
+function fetchProducts() {
+    fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(data => {
+            allProducts = data; // Store all products
+            displayProducts(data);
+            populateFilterOptions(data);
+        })
+        .catch(error => console.error('Error fetching products:', error));
+}
+
 function getApi(){
     fetch('https://click-shop-project.onrender.com')
     .then(r => r.json())
     .then(d => console.log(d))
 }
 getApi()
+
